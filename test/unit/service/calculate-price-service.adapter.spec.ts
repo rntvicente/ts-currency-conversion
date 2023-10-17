d5c4565290a09131fbea37a89be0906c681b02ca
Renato Vicente <rntvicente@gmail.com>
2023-10-17 17:56:05 -0300

    test: deve retornar 1.99 quando valor informado é 10 e câmbio 5.0174

---
+++ test/unit/service/calculate-price-service.adapter.spec.ts
@@ -0,0 +1,17 @@
+import { CalculatePriceServiceAdapter } from '../../../src/application/services/calculate-price-service.adapter';
+
+import { makeLoggerSUT } from '../../utils/logger-stub';
+
+describe('# Test Unit Calculate Price Service Adapter', () => {
+  const loggerStub = makeLoggerSUT();
+  const sut = new CalculatePriceServiceAdapter(loggerStub);
+
+  it('deve retornar 1.99 quando valor informado é 10 e câmbio 5.0174', () => {
+    const value = 10.0;
+    const exchange = 5.0174;
+    const expected = 1.99;
+
+    const result = sut.execute(value, exchange);
+    expect(result).toStrictEqual(expected);
+  });
+});
