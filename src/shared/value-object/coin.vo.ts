bac3ed00b02b7cd71135e6f8cd60f3e246911ecd
Renato Vicente <rntvicente@gmail.com>
2023-10-16 23:25:05 -0300

    feat: definindo getter

--- src/shared/value-object/coin.vo.ts
+++ src/shared/value-object/coin.vo.ts
@@ -4,14 +4,26 @@ export class CoinVO {
   private _identity: string;
   private _exchange: number;
 
-  constructor(readonly identity: string, readonly exchange: number) {
+  constructor(identity: string, exchange: number) {
     this._identity = identity;
     this._exchange = exchange;
     this.validate();
   }
 
-  validate(): void {
+  private validate(): void {
     if (this._identity.length !== 3) throw new InvalidFieldError("identity");
     if (this._exchange < 0) throw new InvalidFieldError("exchange");
+  }
+
+  get identity() {
+    return this._identity;
+  }
+
+  get exchange() {
+    return this._exchange;
+  }
+
+  get toString() {
+    return `${this._identity} - ${this._exchange}`;
   }
 }
